7fe4ae79736ece32ae1a336a639a638ce9bec798
aadebuger <zhvxxh@gmail.com>
2014-11-22 18:59:50 +0800

    Update pebble-faces.c

--- src/pebble-faces.c
+++ src/pebble-faces.c
@@ -19,7 +19,7 @@ static char *images[] = {
   "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-faces/katherine.png",
   "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-faces/alex.png",
   */
-  "http://shenfengzheng.qiniudn.com/qrcodeh.png"
+  "http://shenfengzheng.qiniudn.com/qrcodeh.pbl.png"
 };
 
 static unsigned long image = 0;
